4ae580b5ed55b8610337d5a3ecf1f6839f711f20
Logic-Dell <inteldell296@gmail.com>
2024-09-18 12:21:04 +0000

    deleted the images that are not being used

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 // sticky navbar
-window.onscroll = function() {
-    var navbar = document.querySelector('nav');
-    if (window.scrollY > 10) {
+// window.onscroll = function() {
+//     var navbar = document.querySelector('nav');
+//     if (window.scrollY > 10) {
+//         navbar.classList.add('scrolled');
+//     } else {
+//         navbar.classList.remove('scrolled');
+//     }
+// };
+
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('nav')
+    if(window.scrollY > 60) {
         navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
     }
-};
+    else {
+        navbar.classList.remove('scrolled')
+    }
+})
